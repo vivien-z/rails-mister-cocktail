1bedcf8a8b6a796cc5367fff4c78928c043e479e
vivien zhu <yuanzyy@gmail.com>
2021-03-25 22:21:33 -0400

    created seed file with 2 users and 7 cocktails

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,7 @@ require 'json'
 puts "destroying records..."
 
 Ingredient.destroy_all
+User.destroy_all
 
 puts "start seeding..."
 
@@ -19,43 +20,43 @@ ingredients.each do |i|
 end
 
 # create users
-user_1 = User.create!(username: "mango", email: "a@cocktail.com", password: "123456")
-user_2 = User.create!(username: "dingolingo", email: "b@cocktail.com", password: "123456")
+user_1 = User.create!(username: "minomi", email: "m@cocktail.com", password: "123456")
+user_2 = User.create!(username: "dingolingo", email: "d@cocktail.com", password: "123456")
 
 # create cocktails
 cocktail_1 = Cocktail.new(name: "Mojito")
 cocktail_1.user = user_1
-cocktail_1.photo.attach(io: file, filename: 'mojito.jpg', content_type: 'image/jpg')
+cocktail_1.photo.attach(io: File.open("app/assets/images/mojito.jpg"), filename: 'mojito.jpg', content_type: 'image/jpg')
 cocktail_1.save!
 
 cocktail_2 = Cocktail.new(name: "Pink Lady")
 cocktail_2.user = user_1
-cocktail_2.photo.attach(io: file, filename: 'pink-lady.jpg', content_type: 'image/jpg')
+cocktail_2.photo.attach(io: File.open("app/assets/images/pink-lady.jpg"), filename: 'pink-lady.jpg', content_type: 'image/jpg')
 cocktail_2.save!
 
 cocktail_3 = Cocktail.new(name: "Long Island Iced Tea")
 cocktail_3.user = user_2
-cocktail_3.photo.attach(io: file, filename: 'long-island.jpg', content_type: 'image/jpg')
+cocktail_3.photo.attach(io: File.open("app/assets/images/long-island.jpg"), filename: 'long-island.jpg', content_type: 'image/jpg')
 cocktail_3.save!
 
 cocktail_4 = Cocktail.new(name: "Pina Colada")
 cocktail_4.user = user_2
-cocktail_4.photo.attach(io: file, filename: 'pina-colada.jpg', content_type: 'image/jpg')
+cocktail_4.photo.attach(io: File.open("app/assets/images/pina-colada.jpg"), filename: 'pina-colada.jpg', content_type: 'image/jpg')
 cocktail_4.save!
 
 cocktail_5 = Cocktail.new(name: "Margarita")
 cocktail_5.user = user_1
-cocktail_5.photo.attach(io: file, filename: 'margarita.jpg', content_type: 'image/jpg')
+cocktail_5.photo.attach(io: File.open("app/assets/images/margarita.jpg"), filename: 'margarita.jpg', content_type: 'image/jpg')
 cocktail_5.save!
 
 cocktail_6 = Cocktail.new(name: "Mai Tai")
 cocktail_6.user = user_1
-cocktail_6.photo.attach(io: file, filename: 'mai-tai.jpg', content_type: 'image/jpg')
+cocktail_6.photo.attach(io: File.open("app/assets/images/mai-tai.jpg"), filename: 'mai-tai.jpg', content_type: 'image/jpg')
 cocktail_6.save!
 
 cocktail_7 = Cocktail.new(name: "Blue Ocean")
 cocktail_7.user = user_1
-cocktail_7.photo.attach(io: file, filename: 'blue.jpg', content_type: 'image/jpg')
+cocktail_7.photo.attach(io: File.open("app/assets/images/blue.jpg"), filename: 'blue.jpg', content_type: 'image/jpg')
 cocktail_7.save!
 
 puts "done seeding..."
